bbcdf08a30be210cbaf3cdca2b422eaedd5a1a98
Anish <sharmaanish256@gmail.com>
2022-05-13 13:00:38 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# Crypto Top 10
+
+A basic web app created using react that displays the top 10 current cryptocurrencies according to Market Cap.
+
+A free API by Coingecko has been used to fetch the data.
+
+
+Currently deployed at: 
+https://clinquant-fudge-e99626.netlify.app/
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Box from "./components/box"
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          CryptoTop10
+         
+        </h1>
+        <Box/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
